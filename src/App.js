83a93a6fa4c82a79681d4017333527c231207e55
John Carl Cueva <ninjadentist24@gmail.com>
2024-08-19 08:41:56 +0800

    fix background

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Navbar from "./components/Navbar";
 export default function App() {
     return (
         <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-            <div className="fixed top-0 -z-10 w-full h-full">
+            <div className="fixed top-0 -z-10 w-full h-screen">
                 <div className="relative h-full w-full bg-gradient-to-b from-slate-950 via-slate-950 to-purple-950"><div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>
             </div>
             
