3666c4088f2062164bd5e73e0c20ef469e797cdc
Adzeela <admin@adzeela.com>
2025-08-14 15:21:27 +0800

    k-egg

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,8 @@ import FCLO from "../assets/project-thumbnails/FCLO.png";
 import SP from "../assets/project-thumbnails/SP.png";
 import SIB from "../assets/project-thumbnails/SIB.png";
 import YTK2 from "../assets/project-thumbnails/YTK2.png";
+import KEGG from "../assets/project-thumbnails/KEGG.png";
+
 import { motion } from "framer-motion";
 
 export default function Projects(){
@@ -88,6 +90,14 @@ const projects = [
         isDeployed: true,
         link: "https://ytkaraoke.vercel.app/",
     },
+    {
+        title: "K-Egg Website",
+        thumbnail: KEGG, // Replace with actual thumbnail variable or image
+        description: "A sample website designed for a K-Egg franchise, showcasing menu items, store locations, promotions, and brand story. Built with a clean, modern design and responsive layout to give potential franchisees a realistic view of how their online presence could look.",
+        technologies: ["HTML", "CSS", "JAVASCRIPT", "REACT.JS", "TAILWIND", "SHADCN"],
+        isDeployed: true,
+        link: "https://k-egg.vercel.app/", // Replace with actual deployment link
+    }
 ];
 
 
